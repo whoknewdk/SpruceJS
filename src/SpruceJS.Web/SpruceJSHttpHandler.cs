129777f99c03501b3839e4ca691c9d637aba082b
Jacob T. Nielsen <jacob@whoknew.dk>
2013-03-19 21:19:34 +0100

    Created asp.net (web) project with requesthandler

---
+++ src/SpruceJS.Web/SpruceJSHttpHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SpruceJS.Web
+{
+	public class SpruceJSHttpHandler : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			
+		}
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+	}
+}
