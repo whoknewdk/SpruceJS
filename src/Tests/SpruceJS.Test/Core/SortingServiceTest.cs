52d2522dac0723a281fef827f7dfeb0dfe6f60ef
Jacob T. Nielsen <jacob@whoknew.dk>
2013-05-09 18:48:43 +0200

    Updated sortingservice unit test to reflect IEnumerable implementation

--- src/Tests/SpruceJS.Test/Core/SortingServiceTest.cs
+++ src/Tests/SpruceJS.Test/Core/SortingServiceTest.cs
@@ -9,30 +9,24 @@ namespace SpruceJS.Test.Core
 		[Fact]
 		public void TopologicalSort()
 		{
-			//var a = new JSModule { Name = "a" };
-			//var b = new JSModule { Name = "b", Dependencies = new[] { "a" } };
-			//var c = new JSModule { Name = "c", Dependencies = new[] { "a", "b" } };
-			//var d = new JSModule { Name = "d", Dependencies = new[] { "a", "e" } };
-			//var e = new JSModule { Name = "e", Dependencies = new[] { "c", "b" } };
+			var a = new JSModule { Name = "a" };
+			var b = new JSModule { Name = "b", Dependencies = new[] { "a" } };
+			var c = new JSModule { Name = "c", Dependencies = new[] { "a", "b" } };
+			var d = new JSModule { Name = "d", Dependencies = new[] { "a", "e" } };
+			var e = new JSModule { Name = "e", Dependencies = new[] { "c", "b" } };
 
-			//IDictionary<string, JSModule> Modules = new Dictionary<string, JSModule>();
-			
-			//Modules.Add("c", c);
-			//Modules.Add("d", d);
+			IDictionary<string, JSModule> Modules = new Dictionary<string, JSModule>();
 
-			//Modules.Add("a", a);
-			//Modules.Add("b", b);
-			//Modules.Add("e", e);
+			Modules.Add("c", c);
+			Modules.Add("d", d);
 
-			//var list = SortingService.Sort(Modules);
+			Modules.Add("a", a);
+			Modules.Add("b", b);
+			Modules.Add("e", e);
 
-			//Assert.Equal(5, list.Count);
+			var list = SortingService.Sort(Modules);
 
-			//Assert.Equal("a", list[0].Name);
-			//Assert.Equal("b", list[1].Name);
-			//Assert.Equal("c", list[2].Name);
-			//Assert.Equal("e", list[3].Name);
-			//Assert.Equal("d", list[4].Name);
+			Assert.Equal(list, new List<JSModule> { a, b, c, e, d });
 		}
 	}
 }
