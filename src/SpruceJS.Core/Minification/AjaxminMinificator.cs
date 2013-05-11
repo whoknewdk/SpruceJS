9aabffaa7129d28a9ff7e2cc0a4efdb79b86633d
Jacob T. Nielsen <jacob@whoknew.dk>
2013-05-11 14:36:23 +0200

    Added Ajaxmin

---
+++ src/SpruceJS.Core/Minification/AjaxminMinificator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Ajax.Utilities;
+
+namespace SpruceJS.Core.Minification
+{
+	public class AjaxminMinificator : IMinificator
+	{
+		Minifier minifier = new Minifier();
+
+		public string Minify(string input)
+		{
+			MemoryStream memoryStream = new MemoryStream();
+			TextWriter tw = new StreamWriter(memoryStream);
+
+			ISourceMap sourcemap = new V3SourceMap(tw);
+
+			var settings = new CodeSettings { SymbolsMap = sourcemap };
+			return minifier.MinifyJavaScript(input, settings);
+		}
+	}
+}
---
+++ src/SpruceJS.Core/Minification/IMinificator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpruceJS.Core.Minification
+{
+	public interface IMinificator
+	{
+		string Minify(string input);
+	}
+}
