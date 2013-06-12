39e30ed1feb1bbdce9cac4e0188457530195c214
Jacob T. Nielsen <jacob@whoknew.dk>
2013-06-12 22:26:08 +0200

    Fixed issue with chrome caching sources

---
+++ src/SpruceJS.Web/UncachedJSHttpHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using SpruceJS.Core.Config;
+
+namespace SpruceJS.Web
+{
+	public class UncachedJSHttpHandler : IHttpHandler
+	{
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.ContentType = "text/javascript";
+
+			// no-cache
+			context.Response.AppendHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+			context.Response.AppendHeader("Pragma", "no-cache"); // HTTP 1.0.
+			context.Response.AppendHeader("Expires", "0"); // Proxies.
+
+			// Output
+			context.Response.Write(File.ReadAllText(context.Request.PhysicalPath));
+		}
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+	}
+}
