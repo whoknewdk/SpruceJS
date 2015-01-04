155f47ea49dc9d0b2daf90f684857cf223f75eaf
Jacob T. Nielsen <jacob@whoknew.dk>
2015-01-04 14:08:49 +0100

    Added nuget package source

--- src/SpruceJS/Properties/AssemblyInfo.cs
+++ src/SpruceJS/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SpruceJS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A JavaScript module assembler for ASP.NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jacob T. Nielsen")]
 [assembly: AssemblyProduct("SpruceJS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.*")]
