79895f5b69c5774b08b9c0aa67098d99ee8aa032
Jacob T. Nielsen <jacob@whoknew.dk>
2015-02-22 21:17:38 +0100

    Corrected wording

--- src/SpruceJS/Core/Modules/Exceptions/ModuleKeyDoesNotExistException.cs
+++ src/SpruceJS/Core/Modules/Exceptions/ModuleKeyDoesNotExistException.cs
@@ -4,7 +4,7 @@ namespace SpruceJS.Core.Modules.Exceptions
 	public class ModuleKeyDoesNotExistException : SpruceModuleException
 	{
 		public ModuleKeyDoesNotExistException(string name, string url)
-			: base("No module named \"" + name + "\" exists. Used in \"" + url + "\" exists")
+			: base("No module named \"" + name + "\" exists. Used in \"" + url + "\".")
 		{
 			
 		}
